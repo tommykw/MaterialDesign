3866bced3d53f7227193c5d97cb186461b136fa0
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-12-19 20:58:01 +0900

    Add layout tag

--- ViewPager/app/src/main/java/tokyo/tommykw/viewpager/MainActivity.java
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/MainActivity.java
@@ -4,6 +4,8 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import tokyo.tommykw.viewpager.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
