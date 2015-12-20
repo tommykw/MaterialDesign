8acb3cf7ae8c00d5b5edff88023a347932d01409
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-12-20 10:56:51 +0900

    Add adater and fragment

---
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ScreenSlidePagerAdapter.java
@@ -0,0 +1,24 @@
+package tokyo.tommykw.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by tommy on 15/12/20.
+ */
+public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+    public ScreenSlidePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
