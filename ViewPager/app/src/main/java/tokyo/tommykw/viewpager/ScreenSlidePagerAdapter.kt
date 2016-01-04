88216c5f068742196424508ac18f1e321a5ac400
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-04 09:17:16 +0900

    Resolved reference databinding

--- ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ScreenSlidePagerAdapter.kt
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ScreenSlidePagerAdapter.kt
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentStatePagerAdapter
  * Created by tommy on 15/12/20.
  */
 class ScreenSlidePagerAdapter(fm: FragmentManager, private val mNumPages: Int) : FragmentStatePagerAdapter(fm) {
-
     override fun getItem(position: Int): Fragment? {
         return null
     }
