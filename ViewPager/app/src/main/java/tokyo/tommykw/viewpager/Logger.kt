9ae684f3cadb4feedda83397a40aba5103c1c6f5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-07 23:06:55 +0900

    fix kotlin syntax

--- ViewPager/app/src/main/java/tokyo/tommykw/viewpager/Logger.kt
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/Logger.kt
@@ -10,42 +10,41 @@ import kotlin.properties.Delegates
  * Created by tommy on 15/12/22.
  */
 class Logger(val tag: String, val label: String) {
-    private val mTag: String by lazy { tag }
-    private val mLabel: String by lazy { label }
-    private var mDisabled: Boolean = false
-    internal var mSplits: ArrayList<Long>? = null
-    internal var mSplitLabels: ArrayList<String>? = null
+    private val logTag by lazy { tag }
+    private val logLabel by lazy { label }
+    private var splits = ArrayList<Long>()
+    private var splitLabels = ArrayList<String>()
+    private var disabled = false
 
     fun reset() {
-        mDisabled = !Log.isLoggable(mTag, Log.VERBOSE)
-        if (mDisabled) return
-        if (mSplits == null) {
-            mSplits = ArrayList<Long>()
-            mSplitLabels = ArrayList<String>()
+        disabled = !Log.isLoggable(logTag, Log.VERBOSE)
+        if (disabled) return
+        if (splits == null) {
+            splits = ArrayList()
+            splitLabels = ArrayList()
         } else {
-            mSplits!!.clear()
-            mSplitLabels!!.clear()
+            splits.clear()
+            splitLabels.clear()
         }
     }
 
     fun addSplit(splitLabel: String) {
-        if (mDisabled) return
-        val now = SystemClock.elapsedRealtime()
-        mSplits!!.add(now)
-        mSplitLabels!!.add(splitLabel)
+        if (disabled) return
+        splits.add(SystemClock.elapsedRealtime())
+        splitLabels.add(splitLabel)
     }
 
     fun dump() {
-        if (mDisabled) return
-        Log.d(mTag, mLabel!! + ":begin")
-        val first = mSplits!![0]
+        if (disabled) return
+        Log.d(logTag, logLabel + ":begin")
+        val first = splits[0]
         var now = first
-        for (i in 1..mSplits!!.size - 1) {
-            now = mSplits!![i]
-            val splitLabel = mSplitLabels!![i]
-            val prev = mSplits!![i - 1]
-            Log.d(mTag, mLabel + ":    " + (now - prev) + " ms, " + splitLabel)
+        for (i in 1..splits.size - 1) {
+            now = splits[i]
+            val prev = splits[i - 1]
+            val splitLabel = splitLabels[i]
+            Log.d(logTag, logTag + ":    " + (now - prev) + " ms, " + splitLabel)
         }
-        Log.d(mTag, mLabel + ": end, " + (now - first) + " ms")
+        Log.d(logTag, logLabel + ": end, " + (now - first) + " ms")
     }
 }
