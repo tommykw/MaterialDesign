1f0cfa992e956cd7ce1da8fe6f360eabcec6afd8
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-15 13:51:48 +0900

    Started implementing EnumSetConverter

--- ViewPager/app/src/main/java/tokyo/tommykw/viewpager/app/EnumSetConverter.kt
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/app/EnumSetConverter.kt
@@ -1,7 +1,13 @@
 package tokyo.tommykw.viewpager.app
 
+import java.util.*
+
 /**
  * Created by tommy on 16/01/14.
  */
-class EnumSetConverter {
+class EnumSetConverter<T : EnumSet<T>>() {
+    fun convert(enumSet: EnumSet<T>): String {
+        Class<?> innerType = enumSet.iterator().next().getClass();
+    }
 }
+
