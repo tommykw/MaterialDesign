c3f654088d3c81ce78678422fe2482b0ad88d53d
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-13 11:15:46 +0900

    Implmenet custom view

--- ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ui/widget/LightSaberView.kt
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ui/widget/LightSaberView.kt
@@ -1,10 +1,22 @@
 package tokyo.tommykw.viewpager.ui.widget
 
+import android.content.Context
+import android.util.AttributeSet
 import android.view.View
 
 /**
  * This view is custom view extend View
  */
-class LightSaberView {
+class LightSaberView : View {
+    constructor(context: Context) : super(context) {
 
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
 }
