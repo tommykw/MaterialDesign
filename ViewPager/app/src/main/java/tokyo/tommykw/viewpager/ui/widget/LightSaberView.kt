815062a9f7296dc25561ce96c3968a299e31002b
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-13 14:21:55 +0900

    Done view

--- ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ui/widget/LightSaberView.kt
+++ ViewPager/app/src/main/java/tokyo/tommykw/viewpager/ui/widget/LightSaberView.kt
@@ -1,8 +1,10 @@
 package tokyo.tommykw.viewpager.ui.widget
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.View
+import tokyo.tommykw.viewpager.R
 
 /**
  * This view is custom view extend View
@@ -13,10 +15,28 @@ class LightSaberView : View {
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
+        setup(context, attrs)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
 
+    }
+
+    private fun setup(context: Context, attrs: AttributeSet) {
+        var ta = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.LightSaberView,
+                0,
+                0
+        )
+
+        try {
+            val isMaceWindou = ta.getBoolean(R.styleable.LightSaberView_isMaceWindu, false)
+            val force = ta.getInteger(R.styleable.LightSaberView_force, 0);
+        } finally {
+            ta.recycle()
+        }
+
+        setBackgroundColor(R.color.colorPrimary)
     }
 }
