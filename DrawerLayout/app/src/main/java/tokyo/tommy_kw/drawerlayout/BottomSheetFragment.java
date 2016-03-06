9c7c8d20eaa05baec5e2d2e4fe0dd6603d1915d5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-03-06 21:10:54 +0900

    Add BottomSheet Fragment

---
+++ DrawerLayout/app/src/main/java/tokyo/tommy_kw/drawerlayout/BottomSheetFragment.java
@@ -0,0 +1,57 @@
+package tokyo.tommy_kw.drawerlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by tommy on 2016/03/06.
+ */
+public class BottomSheetFragment extends Fragment {
+    private static final String ID_LIST = "id_list";
+    private RecyclerView recyclerView;
+
+    public static BottomSheetFragment newInstance() {
+        return new BottomSheetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+
+        TextView addButton = (TextView) view.findViewById(R.id.add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView removeButton = (TextView) view.findViewById(R.id.remove);
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        final SwipeRefreshLayout swipeRefresh = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh);
+        swipeRefresh.setOnClickListener(new SwipeRefreshLayout.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
